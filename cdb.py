b4660f23bd87a90a417cc09387c437674d0c8e94
yonsson <yonsson@gmail.com>
2020-11-22 01:46:37 +0300

    fix funccall

--- cdb.py
+++ cdb.py
@@ -23,13 +23,13 @@ async def start(event):
 @bot.on(events.NewMessage(pattern='всё|все|Всё|Все|all|All'))
 async def echoall(event):
     """Echo all drop."""
-    await event.respond(message=f'Наташ, проснись, мы всё уронили!', file=text_to_image(random_catfile(), 'Наташ, проснись, мы всё уронили!'))
+    await event.respond(message=f'Наташ, проснись, мы всё уронили!', file=textimage.text_to_image(random_catfile(), 'Наташ, проснись, мы всё уронили!'))
     raise events.StopPropagation
 
 @bot.on(events.NewMessage)
 async def echo(event):
     """Echo the user message + drop."""
-    await event.respond(message=f'Мы уронили {event.text}', file=text_to_image(random_catfile(), f'Мы уронили {event.text}'))
+    await event.respond(message=f'Мы уронили {event.text}', file=textimage.text_to_image(random_catfile(), f'Мы уронили {event.text}'))
         
 
 def main():
