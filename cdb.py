d4c86c56765e7d141b47c3ef2a00af43957c812c
yonsson <yonsson@gmail.com>
2020-11-21 18:47:30 +0300

    fix getenv to environ

--- cdb.py
+++ cdb.py
@@ -1,13 +1,13 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from telethon import TelegramClient, events
 import random
 
-load_dotenv()
+#load_dotenv()
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-API_ID = os.getenv('API_ID')
-API_HASH = os.getenv('API_HASH')
+BOT_TOKEN = os.environ['BOT_TOKEN']
+API_ID = os.environ['API_ID']
+API_HASH = os.environ['API_HASH']
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
